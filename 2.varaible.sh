43e6acdbefc61940d21f51b81252e2bd4e1ed6c9
Kalyani <kalyani.142011@gmail.com>
2023-12-12 23:36:35 +0530

    my shell scripts

---
+++ 2.varaible.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1::Hellow $PERSON2,Good Morning"
+echo "$PERSON2::Hi $PERSON1,very good Morning"
+echo "$PERSON1::How are you $PEROSN2?"
+echo "$PERSON2:: I am good $PERSON1,How are you?"
+
