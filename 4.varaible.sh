b93563ba624e05db9ab47d99b410c0bb6484416b
Kalyani <kalyani.142011@gmail.com>
2023-12-13 00:12:22 +0530

    my script 4

---
+++ 4.varaible.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1::Hellow $PERSON2,Good Morning"
+echo "$PERSON2::Hi $PERSON1,very good Morning"
+echo "$PERSON1::How are you $PEROSN2?"
+echo "$PERSON2:: I am good $PERSON1,How are you?"
