1e50ac0622a3c3686b71815e56bd6db7d73df59d
Kalyani <kalyani.142011@gmail.com>
2023-12-13 17:21:07 +0530

    my script

---
+++ 5.varaibe.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "please enter your username"
+
+
+read -s USERNAME
+
+echo "please enter your password"
+
+read -s PASSWORD
+
+echo "Username is : $USERNAME, Password is : $PASSWORD"ls
