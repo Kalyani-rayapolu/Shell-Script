3c0fed3c10f65396e24b78a35b9956442f53cdc9
Kalyani <kalyani.142011@gmail.com>
2023-12-18 14:37:36 +0530

    my logs

--- 15.delete-log.sh
+++ 15.delete-log.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-SORCE_DIR=/temp/shellsccripts-logs
+SORCE_DIR="/temp/shellsccript-logs"
 R="\e[31m"
 G="\e[32m"
 Y="\e[31m"
-N= "\e[0m"
+N="\e[0m"
 
 if [ ! -d $SOURCE_DIR ] 
 then
     echo -e "$R Source directory: $SOURCE_DIR does not exists $N"
-
 fi
+
+
+FILES_TO_DELETE=$(find $SORCE_DIR -type f -mtime +14 -name "*.log")
+
+while IFS= read -r lie
+do
+  echo "Deleting file: $line"
+  rm -rf $line
+done <<< $FILES_TO_DELETE
